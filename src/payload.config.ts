1a209c11b84b6e5c4c6f4a9f48b4c4b23d548161
Yves Tscherry <info@feerglas.com>
2025-04-29 16:14:16 +0200

    add and configure storage-vercel-blob, add minial db implementation

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,11 +1,11 @@
 // storage-adapter-import-placeholder
 import { mongooseAdapter } from '@payloadcms/db-mongodb'
-import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
 import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
 import sharp from 'sharp'
+import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob'
 
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
@@ -31,7 +31,14 @@ export default buildConfig({
   }),
   sharp,
   plugins: [
-    payloadCloudPlugin(),
-    // storage-adapter-placeholder
+    vercelBlobStorage({
+      access: 'public',
+      clientUploads: true,
+      collections: {
+        [Media.slug]: true,
+      },
+      enabled: true,
+      token: process.env.BLOB_READ_WRITE_TOKEN,
+    }),
   ],
 })
